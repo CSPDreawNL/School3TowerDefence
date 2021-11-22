4dccb1ad9778bf834957c6240c204f6eda716001
BoazBaaz <Boas.Bas@Gmail.com>
2021-11-22 10:44:11 +0100

    TowerBuilder.cs changes
    You can place towers now. They can still spawn in each other and they spawn to low.

--- TowerDefence3/Assets/Scripts/TowerBuilder.cs
+++ TowerDefence3/Assets/Scripts/TowerBuilder.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TowerBuilder : MonoBehaviour {
-
-    [SerializeField] private Vector3 m_MouseInWorldPosition;
     [SerializeField] private SOTower m_CurrentTower;
+
+    [Header("Info")]
+    [SerializeField] private KeyCode m_PlaceTowerKey = KeyCode.Mouse0;
+    [SerializeField] private LayerMask m_RaycastLayerMask;
 
 
     void Update() {
-        m_MouseInWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        if (Input.GetKeyDown(KeyCode.Mouse0)) {
-            Instantiate(m_CurrentTower.towerPrefab, m_MouseInWorldPosition, Quaternion.identity);
+        if (Input.GetKeyDown(m_PlaceTowerKey)) {
+            PlaceTower();
         }
+    }
+
+    private void PlaceTower() {
+
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(GetMouseRay(), out hit, float.PositiveInfinity , m_RaycastLayerMask.value);
+
+        //Has hit the surface of an object.
+        if (hasHit) {
+            Instantiate(m_CurrentTower.towerPrefab, hit.point, Quaternion.identity);
+        }
+    }
+
+    private static Ray GetMouseRay() {
+        return Camera.main.ScreenPointToRay(Input.mousePosition);
     }
 }
