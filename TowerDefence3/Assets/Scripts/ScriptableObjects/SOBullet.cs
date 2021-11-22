2defb57cf6810e8bfca436e356143fe9937f20e0
Dreaw Riddleton <dreawriddleton@hotmail.com>
2021-11-22 10:33:12 +0100

    Bullet scriptable object added

---
+++ TowerDefence3/Assets/Scripts/ScriptableObjects/SOBullet.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Bullet", menuName = "ScriptableObjects/Bullet")]
+public class SOBullet : ScriptableObject
+{
+    public string bulletType;
+    public GameObject bulletPrefab;
+
+    [Header("Info")]
+    public Sprite bulletSprite;
+    public float bulletSpeed;
+
+
+
+}
