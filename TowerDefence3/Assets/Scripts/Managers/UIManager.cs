1e537698ace64d1f057b534d062f3d140956648d
BoazBaaz <Boas.Bas@Gmail.com>
2021-11-30 10:38:00 +0100

    GameScene.unity changes
    Added a pause menu in the GameScene.unity. Also pushed the build settings.

--- TowerDefence3/Assets/Scripts/Managers/UIManager.cs
+++ TowerDefence3/Assets/Scripts/Managers/UIManager.cs
@@ -13,12 +13,23 @@ public class UIManager : MonoBehaviour
             Destroy(this);
     }
 
+    [SerializeField] private KeyCode m_PauseKey = KeyCode.Escape;
     [SerializeField] private GameObject m_GameOverPanel;
     [SerializeField] private GameObject m_YouWonPanel;
+    [SerializeField] private GameObject m_PausePanel;
+
+    private bool isGamePaused = false;
 
     private void Start() {
         m_GameOverPanel.SetActive(false);
         m_YouWonPanel.SetActive(false);
+        m_PausePanel.SetActive(false);
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(m_PauseKey)) {
+            PauseGame();
+        }
     }
 
     public void GameOver() {
@@ -29,5 +40,19 @@ public class UIManager : MonoBehaviour
     public void YouWon() {
         m_YouWonPanel.SetActive(true);
         Time.timeScale = 0f;
+    }
+
+    public void PauseGame() {
+        if (!isGamePaused) {
+            m_PausePanel.SetActive(true);
+            isGamePaused = true;
+            Time.timeScale = 0f;
+        }
+        else if (isGamePaused) {
+            m_PausePanel.SetActive(false);
+            isGamePaused = false;
+            Time.timeScale = 1f;
+        }
+
     }
 }
